4605f9a7b1def4dde0edbbff63be95823090b952
joedunlap <joedunlap26@gmail.com>
2024-04-25 17:11:14 -0500

    added copy density button, logo, messed with css some

--- script.js
+++ script.js
@@ -6,5 +6,25 @@ function calculateDensity() {
     const averageDensity = ((rep1 * 2) + (rep2 * 2) + (rep3 * 2)) / 3;
   
     document.getElementById('result').innerText = `Average Density: ${averageDensity.toFixed(2)} g/ml`;
+  };
+
+
+  function copyDensity() {
+    const densityText = document.getElementById('result').innerText;
+    const densityValue = densityText.split(': ')[1].split(' ')[0]; // Extract numeric part
+    
+    navigator.clipboard.writeText(densityValue)
+      .then(() => {
+        console.log('Density copied to clipboard:', densityValue);
+        // Optionally, provide user feedback that the value has been copied
+      })
+      .catch(err => {
+        console.error('Unable to copy density to clipboard:', err);
+        // Handle any errors, such as browser compatibility or permission issues
+      });
   }
+
+
   
+  
+
