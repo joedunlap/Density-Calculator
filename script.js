57fb1deab515ed2291382594a4f7a85639726073
joedunlap <joedunlap26@gmail.com>
2024-05-01 15:51:17 -0500

    changed copyDensity function so that it can work on http and not be blocked by the clipboard being disabled

--- script.js
+++ script.js
@@ -1,38 +1,48 @@
 function calculateDensity() {
-    const rep1 = parseFloat(document.getElementById('rep1').value);
-    const rep2 = parseFloat(document.getElementById('rep2').value);
-    const rep3 = parseFloat(document.getElementById('rep3').value);
-  
-    const averageDensity = ((rep1 * 2) + (rep2 * 2) + (rep3 * 2)) / 3;
-  
-    document.getElementById('result').innerText = `Average Density: ${averageDensity.toFixed(2)} g/ml`;
-  };
+  const rep1 = parseFloat(document.getElementById('rep1').value);
+  const rep2 = parseFloat(document.getElementById('rep2').value);
+  const rep3 = parseFloat(document.getElementById('rep3').value);
 
+  const averageDensity = ((rep1 * 2) + (rep2 * 2) + (rep3 * 2)) / 3;
 
-  function copyDensity() {
-    const densityText = document.getElementById('result').innerText;
-    const densityValue = densityText.split(': ')[1].split(' ')[0]; // Extract numeric part
-    
-    navigator.clipboard.writeText(densityValue)
-      .then(() => {
-        console.log('Density copied to clipboard:', densityValue);
-        // Optionally, provide user feedback that the value has been copied
-      })
-      .catch(err => {
-        console.error('Unable to copy density to clipboard:', err);
-        // Handle any errors, such as browser compatibility or permission issues
-      });
-  };
+  document.getElementById('result').innerText = `Average Density: ${averageDensity.toFixed(2)} g/ml`;
+};
 
-  function clearInputs() {
-    document.getElementById('rep1').value = '';
-    document.getElementById('rep2').value = '';
-    document.getElementById('rep3').value = '';
-    document.getElementById('result').innerText = '';
+function unsecuredCopyToClipboard(text) {
+  const textArea = document.createElement("textarea");
+  textArea.value = text;
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+  try {
+    document.execCommand('copy');
+  } catch (err) {
+    console.error('Unable to copy to clipboard', err);
+  }
+  document.body.removeChild(textArea);
 };
 
 
 
-  
-  
+function copyDensity() {
+  const densityText = document.getElementById('result').innerText;
+  const densityValue = densityText.split(': ')[1].split(' ')[0]; // Extract numeric part
+
+  unsecuredCopyToClipboard(densityValue)
+  console.log('Density copied to clipboard:', densityValue);
+  // Optionally, provide user feedback that the value has been copied
+
+};
+
+function clearInputs() {
+  document.getElementById('rep1').value = '';
+  document.getElementById('rep2').value = '';
+  document.getElementById('rep3').value = '';
+  document.getElementById('result').innerText = '';
+};
+
+
+
+
+
 
